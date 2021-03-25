644592df9446fa95244656626630bd0968bb9c53
Marco Arboleda <arboleda.marco@gmail.com>
2021-03-25 19:31:35 -0400

    Create initial code for Projects section

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
-import About from './components/About'
+import About from './components/About';
+import Projects from './components/Projects';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <About></About>
+      <Projects></Projects>
     </div>
   );
 }
---
+++ src/components/Projects.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import inventory_app_preview from '../images/inventory_app_preview.png';
+
+const ProjectsContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+const Project = styled.div`
+    width: 80%;
+    display: grid;
+    grid-template-columns: 420px 1fr;
+    grid-template-rows: 1fr 820px;
+    grid-auto-flow: column;
+`;
+
+const ProjectDescription = styled.p`
+    grid-row: span 2;
+`;
+
+const Projects = () => {
+    return (
+        <ProjectsContainer>
+            <Project>
+                <h3>Inventory App</h3>
+                <img src={inventory_app_preview} alt='inventory app&quot;s home page' />
+                <ProjectDescription>This is my inventory app, etc., etc.</ProjectDescription>
+            </Project>
+        </ProjectsContainer>
+    )
+}
+
+export default Projects;
